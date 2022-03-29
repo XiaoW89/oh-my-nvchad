ea716170079cea35a6b27c9a2045f9dfe0b69d9e
wangbin <wangbin@zerozero.cn>
2022-03-29 02:54:46 +0000

    20220329.修改install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-echo "export LUA_PATH="~/.config/nvim/?.lua;~/.config/nvim/lua/?.lua;~/.config/nvim/lua/?/init.lua;~/.config/nvim/lua/custom/?.lua"" >> ~/.zshrc
+#!/bin/zsh
+echo 'export LUA_PATH="/root/.config/nvim/?.lua;/root/.config/nvim/lua/?.lua;/root/.config/nvim/lua/?/init.lua;/root/.config/nvim/lua/custom/?.lua"'>> ~/.zshrc
 source ~/.zshrc
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
